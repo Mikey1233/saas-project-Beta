40aeb8538722a02c0e75d11162e3ea0ff35b9ef6
michael1233 <emmamichael201@gmail.com>
2024-05-22 00:28:23 +0100

    first commit,initialized next app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,54 @@
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+
+// // import { Header } from "@/components/Header";
+// import Header from "@/components/Header";
+// import { ThemeProvider } from "@/components/ThemeProvider";
+// import ClientProviders from "@/components/ClientProviders";
+
+// import "./globals.css";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Chat with Anyone",
+//   description: "Chat with Anyone, Anywhere",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <ClientProviders>
+//       <html lang="en">
+//         <body className={inter.className}>
+//           <ThemeProvider
+//             attribute="class"
+//             defaultTheme="system"
+//             enableSystem
+//             disableTransitionOnChange
+//           >
+//             <Header />
+
+//             {children}
+//           </ThemeProvider>
+//         </body>
+//       </html>
+//     </ClientProviders>
+//   );
+// }
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import { Header } from "@/components/Header";
+import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import ClientProviders from "@/components/ClientProviders";
 
 import "./globals.css";
+// import FirebaseAuthProvider from "@/components/FirebaseAuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,17 +65,19 @@ export default function RootLayout({
   return (
     <ClientProviders>
       <html lang="en">
-        <body className={inter.className}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Header />
+        <body className="flex flex-col min-h-screen">
+          {/* <FirebaseAuthProvider> */}
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Header />
 
-            {children}
-          </ThemeProvider>
+              {children}
+            </ThemeProvider>
+          {/* </FirebaseAuthProvider> */}
         </body>
       </html>
     </ClientProviders>
